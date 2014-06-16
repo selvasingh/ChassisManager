cdd265e18334e7d3f3f7b455b41654b67724feac
YuqingZhaoMSFT <yuqingz@microsoft.com>
2014-06-16 09:47:05 -0700

    fix copyright symbol

--- Test/WcsTestUtil/EnumProp.cs
+++ Test/WcsTestUtil/EnumProp.cs
@@ -1,4 +1,4 @@
-// Copyright ? Microsoft Open Technologies, Inc.
+﻿// Copyright © Microsoft Open Technologies, Inc.
 // All Rights Reserved
 // Licensed under the Apache License, Version 2.0 (the "License"); 
 // you may not use this file except in compliance with the License.
